5b9db20e4ea104f3f0a932c7a8597b4112796a1d
“berlinen” <1292393108@qq.com>
2020-04-24 15:01:25 +0800

    feat: 🌱 base knowledge

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,11 +1,39 @@
 <template>
   <div class="test">
-    <h1>test page</h1>
+    <h1>test count: {{ count }}</h1>
+    <div>count * 2 {{ doubleCount }}</div>
+    <button @click="add">add</button>
+    <br />
+    <br />
+    <br />
+    <br />
+    <div>当前路由：{{ routeName }}</div>
   </div>
 </template>
 
 <script>
+ import { ref, computed, watch, getCurrentInstance } from 'vue'
+
  export default {
+   setup () {
+     const { ctx } = getCurrentInstance()
+     const routeName = ref(ctx.$router.currentRoute.value)
+     const count = ref(0)
+     const add = () => {
+       count.value++
+     }
+     // 监听
+     watch(() => count.value, val => {
+       console.log(`count is ${val}`)
+     })
+     const doubleCount = computed(() => count.value * 2)
+     return {
+       count,
+       add,
+       doubleCount,
+       routeName
+     }
+   }
  }
 </script>
 
