bf7dabdd3be8a16cfbcf87a2dc94dedcbc7434b3
“berlinen” <1292393108@qq.com>
2020-04-24 14:43:04 +0800

    feat: init project

---
+++ src/views/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    <h1>test page</h1>
+  </div>
+</template>
+
+<script>
+ export default {
+ }
+</script>
+
+<style lang="less" scoped>
+.test {
+  color: red;
+}
+</style>
